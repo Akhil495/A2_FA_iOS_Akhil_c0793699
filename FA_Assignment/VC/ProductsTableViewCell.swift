bba3b33baa60cb3af5b37706a086a78cb850d985
Akhil Jain <30870421+Akhil495@users.noreply.github.com>
2021-05-26 21:37:27 +0530

    Add files via upload

---
+++ FA_Assignment/VC/ProductsTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ProductsTableViewCell.swift
+//  A2_FA_iOS_Akhil_c0793699
+//
+//  Created by AKHIL JAIN on 2021-05-25
+//
+
+import UIKit
+
+class ProductsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
